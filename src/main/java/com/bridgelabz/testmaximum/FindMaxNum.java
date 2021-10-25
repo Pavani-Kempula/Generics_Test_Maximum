b0c69c1b9c1ecfeb7493e56328d3f9a2ba3097b6
PavaniKempula <pavanikempula1998@gmail.com>
2021-10-25 18:45:33 +0530

    ADDED : Welcome to Generics Maximum Program

---
+++ src/main/java/com/bridgelabz/testmaximum/FindMaxNum.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.testmaximum;
+
+public class FindMaxNum {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Generics Maximum Program");
+    }
+}
