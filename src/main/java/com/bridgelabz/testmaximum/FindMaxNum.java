5b97b8f3f62c7f7e9bb89b9421150f1244cb27a8
PavaniKempula <pavanikempula1998@gmail.com>
2021-10-31 21:35:45 +0530

    REFACTORED : Made class generic type declared three generic type class level variables

--- src/main/java/com/bridgelabz/testmaximum/FindMaxNum.java
+++ src/main/java/com/bridgelabz/testmaximum/FindMaxNum.java
@@ -1,22 +1,40 @@
 package com.bridgelabz.testmaximum;
 
-public class FindMaxNum {
-    /*
-     * Name : maximumInteger
-	 *
-	 * Description : Finding maximum number
-	 *
-	 * Algorithm : Finding max integer using compareTo() method
-     */
-    public <E extends Comparable> E findMax(E num1, E num2, E num3) {
-        E maxNumber = num1;
-        if (num2.compareTo(maxNumber) > 0){
-            maxNumber = num2;
+/*
+ * Name : maximumInteger
+ *
+ * Description : Finding maximum number
+ *
+ * Algorithm : Finding maximum numbers using generic
+ */
+
+public class FindMaxNum<E extends  Comparable<E>> {
+    E value1;
+    E value2;
+    E value3;
+
+    public FindMaxNum(E value1, E value2, E value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public FindMaxNum() {
+
+    }
+
+    public E findMax() {
+        return FindMaxNum.findMax(value1, value2, value3);
+    }
+
+    public static <E extends Comparable> E findMax(E value1, E value2, E value3) {
+        E maxNumber = value1;
+        if (value2.compareTo(maxNumber) > 0) {
+            maxNumber = value2;
         }
-        if (num3.compareTo(maxNumber) > 0){
-            maxNumber = num3;
+        if (value3.compareTo(maxNumber) > 0) {
+            maxNumber = value3;
         }
         return maxNumber;
     }
-
 }
